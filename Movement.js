e05e67840601995032008460f0bb0dd78e3133d5
JamxsBart <140333958+JamxsBart@users.noreply.github.com>
2026-01-08 18:06:22 +0000

    Update2
    JS CSS HTML

---
+++ Movement.js
@@ -0,0 +1,14 @@
+function scrollToModels1() {
+    var section1 = document.getElementById("section1");
+    section1.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function scrollToModels2() {
+    var section2 = document.getElementById("section2");
+    section2.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function scrollToModels3() {
+    var container1 = document.getElementById("container1");
+    container1.scrollIntoView({ behavior: 'smooth' });
+  }
